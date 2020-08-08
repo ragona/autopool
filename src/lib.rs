79a96b937015f99557ce1953cdea0e26d81edaa8
Ryan Ragona <ryanragona@gmail.com>
2020-08-08 07:22:52 -0700

    fixed import error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # autopool
 //!
-//! > :warning: **Project in early development!** This is not a stable library.
+//! > Warning! **Project in early development!** This is not a stable library.
 //!
 //! This is an attempt to use a [PID controller](https://en.wikipedia.org/wiki/PID_controller) to drive a worker pool.
 //! Most worker pools require you to set a number of workers.
@@ -19,9 +19,11 @@ mod tracker;
 #[cfg(feature = "tuning")]
 pub mod tuning;
 
-pub use autopool::{AutoPool, AutoPoolConfig};
-pub use pid::PidController;
-pub use pool::{Job, JobStatus, WorkerPool, WorkerPoolCommand, WorkerPoolConfig};
+pub use crate::{
+    autopool::{AutoPool, AutoPoolConfig},
+    pid::PidController,
+    pool::{Job, JobStatus, WorkerPool, WorkerPoolCommand, WorkerPoolConfig},
+};
 
 pub(crate) use crossbeam_channel::{
     self, Receiver as CrossbeamReceiver, Sender as CrossbeamSender,
